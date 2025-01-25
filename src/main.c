ff9bd2f1920ed242ffef3c5210090f5921f6b123
FatherOfGloom <antonuh2002@gmail.com>
2025-01-25 03:45:24 +0300

    initial commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unholy.h>
+
+// a dynamic number representation
+typedef struct DynNum {
+    Vec v;
+    u8 base;
+} DynNum;
+
+int main(void) {
+    printf("Ur mom\n");
+    return 0;
+}
